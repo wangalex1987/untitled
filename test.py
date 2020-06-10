cadbefca8cf617fb9c8a095ab9b5e7545bdcb6d6
wangalex1987 <951914201@qq.com>
2020-06-10 20:52:20 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import requests
+
+session=requests.Session()
+verify="http://localhost/index.php?m=Home&c=User&a=verify"
+login="http://localhost/index.php?m=Home&c=User&a=do_login"
+response_verify = session.get(verify)
+data={"username": "13761263624", "password": "123456", "verify_code": "8888"}
+response_login = session.post(url=login,data=data)
+print(response_login.json())
+session.close()
