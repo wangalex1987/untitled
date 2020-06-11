801ab5394d8592aca8e5de60632167e63ca3893f
wangalex1987 <951914201@qq.com>
2020-06-11 10:39:34 +0800

    运行插件

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import requests,unittest
+from parameterized import parameterized
+
+
 class test_login(unittest.TestCase):
-    def login(self):
+    p_list=[("13761263624","123456","8888")]
+    @parameterized.expand(p_list)
+    def test_login(self,username,password,verify_code):
         session=requests.Session()
         verify="http://localhost/index.php?m=Home&c=User&a=verify"
         login="http://localhost/index.php?m=Home&c=User&a=do_login"
         response_verify = session.get(verify)
-        data={"username": "13761263624", "password": "123456", "verify_code": "8888"}
+        data={"username":username, "password": password, "verify_code": verify_code}
         response_login = session.post(url=login,data=data)
         print(response_login.json())
         session.close()
